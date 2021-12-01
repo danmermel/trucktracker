c83b02eeca000f8bd08c3690f74e22482a097d62
Daniel Mermelstein <daniel.mermelstein@ibm.com>
2021-12-01 14:39:32 +0000

    creates a db if it does not already exist

--- cloudantConsumer.js
+++ cloudantConsumer.js
@@ -1,18 +1,18 @@
 // import the `Kafka` instance from the kafkajs library
 const { Kafka } = require("kafkajs")
 const topic = 'es-topic'
-const creds = require ("./creds.json")
+const creds = require("./creds.json")
 
 //create kafka object with access credentials
 const kafka = new Kafka({
 	clientId: 'my-app',
 	brokers: [creds.eventstreams_credentials.value["kafka_brokers_sasl.0"],
-			creds.eventstreams_credentials.value["kafka_brokers_sasl.1"],
-			creds.eventstreams_credentials.value["kafka_brokers_sasl.2"],
-			creds.eventstreams_credentials.value["kafka_brokers_sasl.3"],
-			creds.eventstreams_credentials.value["kafka_brokers_sasl.4"],
-			creds.eventstreams_credentials.value["kafka_brokers_sasl.5"]
-		
+	creds.eventstreams_credentials.value["kafka_brokers_sasl.1"],
+	creds.eventstreams_credentials.value["kafka_brokers_sasl.2"],
+	creds.eventstreams_credentials.value["kafka_brokers_sasl.3"],
+	creds.eventstreams_credentials.value["kafka_brokers_sasl.4"],
+	creds.eventstreams_credentials.value["kafka_brokers_sasl.5"]
+
 	],
 	// authenticationTimeout: 1000,
 	// reauthenticationThreshold: 10000,
@@ -29,47 +29,69 @@ const { CloudantV1 } = require('@ibm-cloud/cloudant')
 
 const { IamAuthenticator } = require('ibm-cloud-sdk-core');
 const authenticator = new IamAuthenticator({
-    apikey: creds.cloudant_credentials.value.apikey
+	apikey: creds.cloudant_credentials.value.apikey
 });
 const cloudant = new CloudantV1({
-    authenticator: authenticator
+	authenticator: authenticator
 });
 cloudant.setServiceUrl(creds.cloudant_credentials.value.url);
 
+//create db if it doesn't exist already
+const dbName = "trucktracker"
+const createDb = cloudant
+	.putDatabase({ db: dbName })
+	.then((putDatabaseResult) => {
+		if (putDatabaseResult.result.ok) {
+			console.log(`"${dbName}" database created."`);
+		}
+	})
+	.catch((err) => {
+		if (err.code === 412) {
+			console.log(
+				'Cannot create "' + dbName + '" database, it already exists.'
+			);
+		}
+	});
 
 const consumer = kafka.consumer({ groupId: 'cloudant' })
 
-const run = async () => {
-  await consumer.connect()
-  await consumer.subscribe({ topic
-							//, fromBeginning: true
-						 })
-  await consumer.run({
-    eachBatch: async ({ batch }) => {
-	  const batchSize = batch.rawMessages.length
-      console.log("Received a batch of ", batchSize)
-	  batch.rawMessages = batch.rawMessages.map ( function (d) {
-		  let retval = d.value.toString()
-		  //console.log(retval)
-		  retval = JSON.parse(retval)
-		  //console.log(retval)
- 
-		  return retval
 
-	  })
-	  const bulkDocs = {  docs: batch.rawMessages }
-		  
-	  const response = await cloudant.postBulkDocs({
-		db: 'trucktracker',
-		bulkDocs: bulkDocs
-	  })
-	  console.log("response is ", response)
-	  console.log("number of docs uploaded: ", batch.rawMessages.length)
-    },
-   })
+const run = async () => {
+	await consumer.connect()
+	await consumer.subscribe({
+		topic
+		//, fromBeginning: true
+	})
+	await consumer.run({
+
+			eachBatch: async ({ batch }) => {
+				const batchSize = batch.rawMessages.length
+				console.log("Received a batch of ", batchSize)
+				batch.rawMessages = batch.rawMessages.map(function (d) {
+					let retval = d.value.toString()
+					//console.log(retval)
+					retval = JSON.parse(retval)
+					//console.log(retval)
+
+					return retval
+
+				})
+				const bulkDocs = { docs: batch.rawMessages }
+
+				const response = await cloudant.postBulkDocs({
+					db: dbName,
+					bulkDocs: bulkDocs
+				})
+				console.log("response is ", response)
+				console.log("number of docs uploaded: ", batch.rawMessages.length)
+			},
+
+	})
 }
 
+
 run().catch(e => console.error(`[example/consumer] ${e.message}`, e))
+
 
 
 
